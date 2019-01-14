0eacefabfc223885771ad954e3322164fb0dbfeb
devoptimist <sbrown@chef.io>
2019-01-14 15:25:23 +0000

    updating with comments

--- builder_air_gapped_bundle_generator.sh
+++ builder_air_gapped_bundle_generator.sh
@@ -1,10 +1,20 @@
+# the name for the archive produced.
 BUNDLE_NAME="${1:-on-prem-builder-bundle}"
+# this version number is used to track structual changes to the generated
+# archive. it does not directly match a version of the builder services.
 BUNDLE_VERSION="v${2:-0.6.0}"
+# get current dir
 S_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# source the utils script, utils contains the commands listed in the main section
 source ${S_DIR}/utils.sh
+
 # top level builder services
-# Salim said to pull from latest, not stable
-# for sup and launcher using latest stable
+# - habitat builder services - Salim said to pull from latest, not stable
+# - devoptimist builder services - using latest stable versios
+#     these are forks of the corrisponding habitat builder services.
+#     The forks demonstrate running the services as non hab and non root users.
+#     This is nessaesry for some customers.
+# - core sup and launcher: using latest stable versions
 SERVICES=(
   core/hab-sup/0.72.0/20190103234046
   core/hab-launcher/9167/20181203215950
@@ -20,12 +30,13 @@ SERVICES=(
   habitat/builder-api/8025/20181221003740
 )
 
-make_pkgs_and_keys_dir
-make_bin_dir
-get_hab_cli
-make_install_script
-main
-archive ${BUNDLE_NAME} ${BUNDLE_VERSION} ${KEY_PATH} ${PKG_PATH} ${BIN_PATH} ${BASE_PATH}/install.sh
-clean_pkgs_and_keys_dir
-clean_bin_dir
-clean_install_file
+# === main section ===#
+make_pkgs_and_keys_dir # creates the harts and keys directorys
+make_bin_dir # creats a directory to store the hab binary
+get_hab_cli # pulls down the lates hab binary
+make_install_script # writes the install script, this is a script which makes sure the correct order of packages are installed
+main # uses the SERVICES array to pull down all the required keys and packages to satisfy all of dependencies and transitive dependencies of each item in the array
+archive ${BUNDLE_NAME} ${BUNDLE_VERSION} ${KEY_PATH} ${PKG_PATH} ${BIN_PATH} ${BASE_PATH}/install.sh # tar gzip the keys hart and bin directorys, and the install.sh file
+clean_pkgs_and_keys_dir # remove the harts and keys directorys
+clean_bin_dir # remove the bin directory
+clean_install_file # remove the install file
